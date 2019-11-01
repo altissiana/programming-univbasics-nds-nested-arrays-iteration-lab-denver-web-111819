7b2e955d65b56cc484b8549e9b6a044da565c573
Tissiana Alves <altissiana@gmail.com>
2019-11-01 23:50:47 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -19,7 +19,24 @@ def find_greater_pair(src)
 end
 
 def total_even_pairs(src)
+  total = 0 
+  for i in 0...src.length
+    number1 = src[i][0]
+    number2 = src[i][1]
+    number1_remainder = number1 % 2
+    number2_remainder = number2 % 2
+    number1_is_even = number1_remainder == 0
+    number2_is_even = number2_remainder == 0
+    if number1_is_even && number2_is_even
+      total += number1 + number2
+  end
   
+  #for i in 0...src.length
+  #  if src[i][0] % 2 == 0 && src[i][1] % 2 == 0
+  #    total += src[i][0] + src[i][1]
+  #  end
+  #end
+
   # src will be an array of [ [number1, number2], ... [numberN, numberM] ]
   # if both numbers in the pair are even, then add both those numbers to the
   # total
