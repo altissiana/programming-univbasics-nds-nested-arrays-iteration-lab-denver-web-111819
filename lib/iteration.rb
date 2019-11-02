3fdf81e518a5d8ea8e933f3eba568243956add91
Tissiana Alves <altissiana@gmail.com>
2019-11-02 00:06:13 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -21,18 +21,19 @@ end
 def total_even_pairs(src)
   total = 0 
   for i in 0...src.length
-    number1 = src[i][0]
-    number2 = src[i][1]
-    number1_remainder = number1 % 2
-    number2_remainder = number2 % 2
-    number1_is_even = number1_remainder == 0
-    number2_is_even = number2_remainder == 0
-    if number1_is_even && number2_is_even
-      total += number1 + number2
-  end
-  #for i in 0...src.length
-  #  if src[i][0] % 2 == 0 && src[i][1] % 2 == 0
-  #    total += src[i][0] + src[i][1]
-  #  end
-  #end
+    if src[i][0] % 2 == 0 && src[i][1] % 2 == 0
+      total += src[i][0] + src[i][1]
+     end
 end
+ 
+  
+  #for i in 0...src.length
+  #number1 = src[i][0]
+  #number2 = src[i][1]
+  #number1_remainder = number1 % 2
+  #number2_remainder = number2 % 2
+  #number1_is_even = number1_remainder == 0
+  #number2_is_even = number2_remainder == 0
+  #if number1_is_even && number2_is_even
+  #total += number1 + number2
+  #end
